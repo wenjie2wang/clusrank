2b394664afe5ed02f1d556001adcaac1c9f3eafd
yuj11004 <jyj.123@live.cn>
2015-11-17 08:32:30 -0500

    Rewrite documentation of functions and data

---
+++ clsrank/R/crd.R
@@ -0,0 +1,15 @@
+#' Clustered non-stratified data
+#'
+#' non-stratified data include 3 variables:
+#'  id, z, group. Have some missing values.
+#'
+#' \itemize{
+#'   \item id. cluster id
+#'   \item z. observed score
+#'   \item group. group id
+#' }
+#'
+#' @format A data frame with 340 rows and 3 variables
+#' @source \url{https://sites.google.com/a/channing.harvard.edu/bernardrosner/channing/incorporating-cluster-effects-for-
+#' the-wilcoxon-rank-sum-test-1/cluswilcox/sample_data}
+"crd"
