9f68bfe0e0bf66bba3524cf3035e1a1752397d47
yuj11004 <jyj.123@live.cn>
2015-11-20 15:34:52 -0500

    Modified ranksum test

--- clsrank/R/ranksum.R
+++ clsrank/R/ranksum.R
@@ -1,6 +1,6 @@
 ################################################################################
 ##
-##   R package clusrank by Mei-Ling Ting Lee, Jun Yan, and Yujing Jiang
+##   R package clusrank by Mei-Lin.csize Tin.csize Lee, Jun Yan, and Yujin.csize Jian.csize
 ##   Copyright (C) 2015
 ##
 ##   This file is part of the R package clusrank.
@@ -16,7 +16,7 @@
 ##   GNU General Public License for more details.
 ##
 ##   You should have received a copy of the GNU General Public License
-##   along with the R package clusrank. If not, see <http://www.gnu.org/licenses/>.
+##   alon.csize with the R package clusrank. If not, see <http://www.gnu.org/licenses/>.
 ##
 ################################################################################
 
@@ -29,18 +29,18 @@
 #'@param formula   an object of class \code{"formula"} in the
 #'form of score ~ group,
 #'@param data a optional data frame
-#'@param subset an optional vector specifying a 
+#'@param subset an optional vector specifyin.csize a 
 #'subset of observations to be used.
-#'@param id a numeric vector indicating the cluster ids of the scores. 
+#'@param id a numeric vector indicatin.csize the cluster ids of the scores. 
 #'If not specified, each score has its own id, i.e., there is no 
 #'cluster in the data.
-#'@param stratum a numeric vector indicating the stratum ids of 
-#'the scores. The default is that the data has no stratum.
+#'@param strats a numeric vector indicatin.csize the strats ids of 
+#'the scores. The default is that the data has no strats.
 #'@param constrasts  the same as that in \code{lm}. Currently unused.
 #'@param na.action a function which indicates what should happen 
 #'when the data contains NAs. The  default action is to omit them.
 #'@param ... additional arguments, currently ignored.
-#'@return  a list with the following components
+#'@return  a list with the followin.csize components
 #'\item{Wc}{Clustered Wilcoxon ranksum statistic.}
 #'\item{ExpWc}{Expected value clustered Wilcoxon ranksum statistic.}
 #'\item{VarWc}{Variance of clustered Wilcoxon ranksum statistic.}
@@ -51,41 +51,39 @@
 #'data(crd)
 #'cluswilcox(z ~ group, data = crd, id = id)
 #'data(crd.str)
-#'cluswilcox(z ~ group, data = crd.str, id = id, stratum = stratum)
+#'cluswilcox(z ~ group, data = crd.str, id = id, strats = strats)
 #'
-#'@author Yujing Jiang 
+#'@author Yujin.csize Jian.csize 
 #'@references
-#'Bernard Rosner, Robert J. Glynn, Mei-Ling Ting Lee(2003)
-#' \emph{Incorporation of Clustering Effects for the Wilcoxon Rank 
+#'Bernard Rosner, Robert J. Glynn, Mei-Lin.csize Tin.csize Lee(2003)
+#' \emph{Incorporation of Clusterin.csize Effects for the Wilcoxon Rank 
 #' Sum Test: A Large-Sample Approach.} Biometrics, \bold{59}, 1089-1098.
 
 
 
-cluswilcox <-
-  function(formula, data = parent.frame(), subset = NULL,
-           id = NULL, stratum = NULL, contrasts = NULL,
-           na.action = na.omit, ...) {
-  # Incoporating clustering effects for the WilcoxonRank Sum Test 
+cluswilcox.test.ranksum <-
+  function(x, cluster, group, strats) {
+  # Incoporatin.csize clusterin.csize effects for the WilcoxonRank Sum Test 
   # for stratified balanced or unbalanced designs. In addition,
-  # one can control for confounding by forming strata which are 
+  # one can control for confoundin.csize by formin.csize strata which are 
   # defined based on cross-classfication of one or more categorical 
-  # covariates which are defined in terms of a single categorical
+  # covariates which are defined in terms of a sin.csizele categorical
   # variable denoted by strata.
   # 
   #
   # Requirement:
   #   An ASCII data file with 4 variables per record. The data file
   #   does not have to be sorted in ID order.
-  #   The 4 variables need to be space selimited and arranged in the 
-  #   following order:
+  #   The 4 variables need to be space selimited and arran.csizeed in the 
+  #   followin.csize order:
   #   1. id
   #   2. score
   #   3. group (X, Y) indicators : need to be 1 and 2.
-  #   4. stratum
+  #   4. strats
   #
   # Args:
   #   1. name of data set.
-  #   2. number of stratum.
+  #   2. number of strats.
   #
   # Returns:
   #   1. Clustered Wilcoxon RankSum Statistic
@@ -99,112 +97,95 @@ cluswilcox <-
     
   METHOD <- "Wilcoxon rank sum test for clutered data"
     
-  cl <- match.call()
-  mf <- match.call(expand.dots = FALSE)
-  
-  DNAME <- paste("from", mf$data)
-  m <- match(c("formula", "data", "subset", "id", "stratum", "na.action"), 
-             names(mf), 0L)
-  mf <- mf[c(1L, m)]
-  mf[[1L]] <- quote(stats::model.frame)
-  mf <- eval(mf, parent.frame())
-  
-  DNAME <- paste(names(mf)[1], DNAME, ", with group id:", names(mf)[2])
-#   if(!is.null(stratum)) {
-#     DNAME <- paste(DNAME, ", with stratum id:", as.character(cl$stratum))
-#   }
-  mt <- attr(mf, "terms")
-  z <- model.response(mf, "numeric")
-  x <- model.matrix(mt, mf, contrasts)
-  group <- x[,2]
-  id <- model.extract(mf, "id")
-  if (is.null(id)) id <- 1:length(z) ## non-clustered data
-  stratum <- model.extract(mf, "stratum")
-  if (is.null(stratum)) stratum <- rep(1, length(z))
-  OK <- complete.cases(z)
-  crd <- data.frame(z, group, id, stratum)[OK,]
-  if (nrow(crd) < 1L) 
-    stop("There is not enough observations")
-  if (!is.numeric(z))
-    stop("The score has to be numeric")
+  data <- as.data.frame(cbind(x, cluster, group, strats))
   ## group is assumed to take value 1 and 2; could be made
   ## more general
-  gr.uniq <- unique(crd$group) ## Record possible groups
+  group.uniq <- unique(data$group) ## Record possible groups
 
-  crd1 <- crd[with(crd, order(id)), ]
+  data <- data[with(data, order(cluster)), ]
   ## Reorder the observations by the order of id.
-  zrank <- rank(crd1$z, na.last = NA)
-  ## Compute the rank of z score
+  xrank <- rank(data$x, na.last = NA)
+  ## Compute the rank of x score
   
-  crd2 <- cbind(subset(crd1, z != "NA"), zrank)
+  data <- cbind(data, xrank)
   ## Add z score to the dataframe crd1
   
-### calculate ranksum within each cluster within each stratum
+### calculate ranksum within each cluster within each strats
   
-  g <- table(crd2$id)
-  ## g is the cluster size
-  sumrank <- c(by(crd2$zrank, crd2$id, sum))
+  cluster.size <- table(data$cluster)
+  ## cluster.size is the cluster size
+  sumrank <- c(by(data$xrank, data$cluster, sum))
   ## Compute rank sum within each cluster
 
-  ## FIXME: unique
-  stratum <- c(by(crd2$stratum, crd2$id, mean))
-  ## Compute stratum mean of each cluster
+  strats <- c(by(data$strats, data$cluster, mean))
+  ## Compute strats  of each cluster
   
-  group <- c(by(crd2$group, crd2$id, mean))
-  ## Compute group mean of each cluster
+  group <- c(by(data$group, data$cluster, mean))
+  ## Compute group  of each cluster
   
   
-  #count number of subunits within cluster size group within each stratum
- 
-  
-  ng.stratum <- as.data.frame(table(g, stratum))
-  ## Count the objects for each group and each stratum 
-  ng.xy <- as.data.frame(table(g, stratum, group))
-  ng.x <- subset(ng.xy, group == 1)
+  #count number of subunits within cluster size group within each strats
+
+  n.csize.strats <- as.data.frame(table(cluster.size, strats))
+  ## Count the objects for each group and each strats 
+  n.csize.xy <- as.data.frame(table(cluster.size, strats, group))
+  n.csize.x <- subset(n.csize.xy, group == 1)
   ## Take out the count for group 1
-  ng.y <- subset(ng.xy, group == 2)
+  n.csize.y <- subset(n.csize.xy, group == 2)
   
-  colnames(ng.stratum)[3] <- "Ngv"
-  colnames(ng.x)[4] <- "mgv"
+  ## Ngv is the number of clusters which have the same 
+  ## subunit, controlled by stratum.
+  colnames(n.csize.strats)[3] <- "Ngv"
   
-  psumrnk <- cbind(g, stratum, group, sumrank)
+  ## mgv is number of clusters which have the same 
+  ## subunit in group x, ontrolled by stratum.
+  colnames(n.csize.x)[4] <- "mgv"
   
-  str_unique <- sort(unique(stratum))
-  str_unique_n <- length(str_unique)
-  g_unique <- unique(g)
-  g_unique_n <- length(g_unique)
-  psumrnk_int <- matrix(0, str_unique_n * g_unique_n, 3)
+  psumrnk <- cbind(cluster.size, strats, group, sumrank)
+  
+  str.uniq <- sort(unique(strats))
+  n.str.uniq <- length(str.uniq)
+  cluser.size.uniq <- unique(cluster.size)
+  n.cluser.size.uniq <- length(cluser.size.uniq)
+  
+  if(n.cluser.size.uniq > 1L) {
+    balance <- FALSE
+  } else {
+    balance <- TRUE
+  }
+  
+  psumrnk_int <- matrix(0,  n.str.uniq * cluser.size.uniq , 3)
   
   k <- 1
-  for(i in 1:str_unique_n){
-    for(j in 1:g_unique_n){
-      ind <- stratum == str_unique[i] & g == g_unique[j]
+  for(i in 1:n.str.uniq ){
+    for(j in 1:n.cluser.size.uniq){
+      ind <- strats == str.uniq[i] & cluster.size == cluser.size.uniq[j]
       foo <- sum(psumrnk[ind, "sumrank"])
       if (foo) {
-        psumrnk_int[k,] <- c(str_unique[i], g_unique[j], foo)
+        psumrnk_int[k,] <- c(str.uniq[i], cluser.size.uniq[j], foo)
         k <- k + 1
       }
     }
   }
-  colnames(psumrnk_int) <- c("stratum", "g", "psumrank")
-  psumrnk_int <- (psumrnk_int[psumrnk_int[, "stratum"] != 0,])
+  colnames(psumrnk_int) <- c("strats", "cluster.size", "psumrank")
+  psumrnk_int <- (psumrnk_int[psumrnk_int[, "strats"] != 0,])
   if (nrow(psumrnk_int) == 1) {
     psumrnk_int <- t(as.data.frame(psumrnk_int))
   } else {
     psumrnk_int <- (as.data.frame(psumrnk_int))
   }
   
-  WC <- sum(psumrnk[psumrnk[,"group"]==1,"sumrank"])
+  WC <- sum(psumrnk[psumrnk[,"group"] == 1,"sumrank"])
   
-  expwc <- merge(merge(ng.x, psumrnk_int), ng.stratum)
+  expwc <- merge(merge(n.csize.x, psumrnk_int), n.csize.strats)
   ExpWc <- sum(expwc[ , "mgv"] * expwc[ , "psumrank"] / expwc[, "Ngv"])
   
-  varwc <- merge(merge(merge(psumrnk, ng.xy), ng.stratum), psumrnk_int)
+  varwc <- merge(merge(merge(psumrnk, n.csize.xy), n.csize.strats), psumrnk_int)
   varwc_int <- (varwc[ ,"sumrank"] - varwc[ ,"psumrank"] / varwc[, "Ngv"])^2
   VarWc <- cbind(varwc, varwc_int)  
   varwc_pre_final <- VarWc[,"Freq"] * (VarWc[, "Ngv"]- VarWc[, "Freq"]) / (VarWc[,"Ngv"] * (VarWc[,"Ngv"] -1)) * VarWc[,"varwc_int"]
   varwc_final <- sum(varwc_pre_final[VarWc[,"Ngv"] > 1])
-  ## Drop the value where there is only 1 cluster in that stratum,
+  ## Drop the value where there is only 1 cluster in that strats,
   ## and that cluster contains only 1
   
   zc <- (WC - ExpWc)/sqrt(varwc_final)
@@ -216,7 +197,7 @@ cluswilcox <-
   result <- list(rstatistic = WC, erstatistic = ExpWc, 
                  vrstatistic = varwc_final,
                  statistic = zc, p.value = pval,
-                 data.name = DNAME, method = METHOD, 
+                 method = METHOD, 
                  balance = balance)
   class(result) <- "ctest"
   result  
