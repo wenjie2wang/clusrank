493eb662ecc7fe8a79248964d664b88452cf4e09
Yujing Jiang <jyj.123@live.cn>
2016-04-27 12:40:53 -0400

    Added data generation function for m groups

--- Rnew/clussimu.R
+++ Rnew/clussimu.R
@@ -4,7 +4,8 @@ clus.simu.2 <- function(M, rho, n, delta, pc) {
     ##
     ## M: A numerical vector with two entries, indicating the number of clusters under each
     ## treatment.
-    ## rho: A numerical vector, the intracluster correlation
+    ## rho: A numerical vector, the intracluster correlation, specific for each cluster size
+    ##      If only one value is supplied, then it is used for all cluster size
     ## for cluster under each treatment, the larger the strong the correlation is.
     ## n: A numerical vector, the cluster sizes.
     ## delta: The location shift between two groups.
@@ -16,6 +17,12 @@ clus.simu.2 <- function(M, rho, n, delta, pc) {
 
     n.grp <- length(M)
     grp.uniq <- c(0 : (n.grp - 1))
+    if(!is.vector(rho)) {
+        rho <- rep(rho, length(n))
+    }
+    if(length(rho) != length(n)) {
+        stop("rho is not of the same length as n")
+    }
     if(is.matrix(pc)) {
         n.in.pc <- nrow(pc)
     } else if(is.vector(pc)) {
@@ -45,7 +52,11 @@ clus.simu.2 <- function(M, rho, n, delta, pc) {
     for( i in 1 : length(M)) {
         n.dist.l[[i]] <- rmultinom(1, M[i], pc[i, ])
         c.size.l[[i]] <- rep.int(n, times = n.dist.l[[i]])
+        
     }
+    c.size <- unlist(c.size.l)
+    c.id <- rep.int(c.id, times = c.size)
+    group <- rep.int(rep.int(grp.uniq, times = M), times = c.size)
     
 
     Y.list <- vector("list", n.grp) ## Generate multinormal data
@@ -69,5 +80,96 @@ clus.simu.2 <- function(M, rho, n, delta, pc) {
     }
             
     X <- unlist(x.list)
-    X
+    data.frame(X, c.id, group)
 }
+
+
+
+clus.simu.m <- function(M, rho, n, delta, pc) {
+    ## Treatments are assigned at cluster level.
+    ##
+    ## M: A numerical vector with two entries, indicating the number of clusters under each
+    ## treatment.
+    ## rho: A numerical vector, the intracluster correlation
+    ## for cluster under each treatment, the larger the strong the correlation is.
+    ## n: A numerical vector, the cluster sizes.
+    ## delta: A vector, the location shift from 0 of the treatment groups, if length is n.grp - 1,
+    ##        the first element is filled with 0.
+    ## pc: A numerical vector or a matrix, the probability distribution
+    ## treatment. If it is a matrix, each row represent a treatment group
+    ##     
+    ## 0 to be assigned to a cluster with a specific size
+    ## Example:  clus.simu.2(c(2, 3), c(0.2, 0.5), c(2, 3), 0.2, 1, 0.2)
+
+    n.grp <- length(M)
+    grp.uniq <- c(1 : (n.grp))
+    if(length(delta) == (n.grp- 1)) {
+        delta <- c(0, delta)
+    }
+    if(length(delta) != n.grp) {
+        stop("delta should have the same length as n")
+    }
+    
+      if(!is.vector(rho)) {
+        rho <- rep(rho, length(n))
+    }
+    if(length(rho) != length(n)) {
+        stop("rho is not of the same length as n")
+    }
+    if(is.matrix(pc)) {
+        n.in.pc <- nrow(pc)
+    } else if(is.vector(pc)) {
+        n.in.pc <- length(M)
+        pc <- replicate(length(M), pc)
+        pc <- t(as.matrix(pc))
+    }
+    if(length(n) == (ncol(pc) - 1)) {
+        pc <- cbind(pc, 1 - colSums(pc))
+    } 
+    
+    if( length(M) != n.in.pc) {
+        stop(" M and pc are not of equal length")
+    }
+
+    n.c.uniq <- length(n)
+    cov.list <- vector("list", n.c.uniq)
+    for( i in 1 : n.c.uniq) {
+        cov.list[[i]] <- (1 - rho[i]) * diag(n[i]) + rho[i] * matrix(1, n[i], n[i])
+    }
+
+    
+    c.id <- c(1 : sum(M)) ## cluster id
+
+    n.dist.l <- c.size.l <-  vector("list", length(M))
+    for( i in 1 : length(M)) {
+        n.dist.l[[i]] <- rmultinom(1, M[i], pc[i, ])
+        c.size.l[[i]] <- rep.int(n, times = n.dist.l[[i]])
+    }
+    c.size <- unlist(c.size.l)
+    c.id <- rep.int(c.id, times = c.size)
+    group <- rep.int(rep.int(grp.uniq, times = M), times = c.size)    
+
+    Y.list <- vector("list", n.grp) ## Generate multinormal data
+    X.list <- vector("list", n.grp) ## STore generate data
+    x.list <- vector("list", n.grp)
+    for( i in 1 : n.grp) {
+        counter <- 1
+        for( j in 1 : length(n)) {
+            if(n.dist.l[[i]][[j]] == 0) {
+               next
+            } else {      
+                Y.list[[i]][[counter]] <- mvrnorm(n.dist.l[[i]][j],
+                                            numeric(n[j]),
+                                            cov.list[[j]])
+                X.list[[i]][[counter]] <- exp(c(t(Y.list[[i]][[counter]]))) +
+                    delta[i]
+                counter <- counter + 1
+            }
+        }
+        x.list[[i]] <- unlist(X.list[[i]])
+    }
+            
+    X <- unlist(x.list)
+    data.frame(X, c.id, group)
+}
+
