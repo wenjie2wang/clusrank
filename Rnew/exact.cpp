aefb5f86c947436918b4198391fc6c6c05412820
Yujing Jiang <jyj.123@live.cn>
2016-05-18 19:21:49 -0400

    Rcpp function of exact test of RGL signed rank test

--- Rnew/exact.cpp
+++ Rnew/exact.cpp
@@ -46,48 +46,50 @@ double pcrksumg(int rks, int I, int J, IntegerVector Score) {
   }
 }
 
-//[[Rcpp::export]]
-int csrk(int srk, int I) {
-
-  if(I < 0) return(0);
-  if(srk < 0) return(0);
-  if( I == 0)  return(1);            
-  if(I > 0) {
-    return(csrk(srk - score_[I - 1], I - 1) + csrk(srk, I - 1));
-  }
-}
 
 
 //[[Rcpp::export]]
+
 int csrkg(int srk, IntegerVector Score) {
-  int N, sum_s, u, c, j;
-  IntegerVector compare(2);
+  /* Count the no of combination with a sum rank less than srk */
+  /* The sum rank is sum(rank[ rank > 0]) in the programme*/
+  /* The input sum rank is sum(sign(rank) * rank) */
+  int N, max_s, sum_s,u, c, j;
+  IntegerVector compare(2), w1;
   N = Score.size();
-  sum_s = sum(Score);
+
   
-  u = sum_s * (sum_s + 1) / 2;
+  max_s = max(Score);
+  sum_s = sum(Score);
+  srk = (sum_s - srk) / 2 + srk; 
+  u = max_s * (max_s + 1) / 2;
   c = (u / 2);
   if( srk < 0 || srk > u)
-    return 0;
-  if( w[0] == 1) {
-    IntegerVector subw(w.begin(), w.begin() + srk + 1);
-    return(sum(subw));
-  }
+    return 0; 
+
   
   compare[1] = c;
-  w = seq_len(sum_s + 1);
-  std::fill(w.begin(), w.end(), 0);
-  w[Score] = 1;
-  w[0] = 1;
+  w = IntegerVector(c + 1);
+  w1 = IntegerVector(c + 1);
+  w[Score[0]] = 1;
+  w[0] = 0;
+
+  w1[Score] = 1;
   Rcpp::Rcout<<w<<std::endl;
-  for( j = 2; j < sum_s + 1; ++j ) {
+
+  for( j = 2; j < N + 1; ++j ) {
     compare[0] = j * (j + 1) / 2;
     int i, end = min(compare);
-    for( i = end; i > j; --i)
-      w[i] += w[i - j];
+    for( i = end; i >= j; --i){
+        w[i] += w[i - j];
+        if(i == j & w1[j] == 1) w[i] += 1;
+        /*  Rcpp::Rcout << "j = " << j << " i = " << i << " w = " << w[i] << std::endl;*/
+    }
   }
-
+  w[0] = 1;
+  Rcpp::Rcout<<w<<std::endl;
   IntegerVector subw(w.begin(), w.begin() + srk + 1);  
   return(sum(subw));
 
 }
+
