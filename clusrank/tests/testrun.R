f7ff8bef7dae19dec5932a66d2b8bc530768b93a
Yujing Jiang <jyj.123@live.cn>
2016-06-01 18:41:38 -0400

    Corrected test functions

--- clusrank/tests/testrun.R
+++ clusrank/tests/testrun.R
@@ -1,10 +1,12 @@
-## Please prepare a full list of test objects and test them one by one
-## The current code does not even run.
-library(clsrank)
+
+
+library(clusrank)
 data(crsd)
-cluswilcox.test(z, cluster = id, data = crsd)
+cluswilcox.test(z, cluster = id, data = crsd, paired = TRUE, method = "rgl")
+cluswilcox.test(z, cluster = id, data = crsd, paired = TRUE, method = "ds")
 data(crsdUnb)
-cluswilcox.test(z, cluster = id, data = crsdUnb)
+cluswilcox.test(z, cluster = id, data = crsdUnb, paired = TRUE, method = "rgl")
+cluswilcox.test(z, cluster = id, data = crsdUnb, paired = TRUE, method = "ds")
 data(crd)
 cluswilcox.test(z ~ group(group) + cluster(id), data = crd)
 data(crdStr)
