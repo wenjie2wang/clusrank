9335c852a9f83c54e9b5927e517c01be38de0647
Yujing Jiang <jyj.123@live.cn>
2016-06-07 11:20:03 -0400

    Debugged simulation code

--- Rnew/clussimu.R
+++ Rnew/clussimu.R
@@ -1,18 +1,17 @@
 library(MASS)
-clus.simu.2 <- function(M, rho, n, delta, pc) {
+clus.simu.rks.2 <- function(M, rho, n, delta, pc) {
     ## Treatments are assigned at cluster level.
     ##
     ## M: A numerical vector with two entries, indicating the number of clusters under each
     ## treatment.
     ## rho: A numerical vector, the intracluster correlation, specific for each cluster size
     ##      If only one value is supplied, then it is used for all cluster size
-    ## for cluster under each treatment, the larger the strong the correlation is.
+    ## for cluster under each treatment.
     ## n: A numerical vector, the cluster sizes.
     ## delta: The location shift between two groups.
-    ## pc: A numerical vector or a matrix, the probability distribution
-    ## treatment. If it is a matrix, each row represent a treatment group
-    ##     
-    ## 0 to be assigned to a cluster with a specific size
+    ## pc: A numerical vector or a matrix, the probability distribution of
+    ## treatment of each cluster size. If it is a matrix, each row represent a treatment group
+    ##  0 to be assigned to a cluster with a specific size
     ## Example:  clus.simu.2(c(2, 3), c(0.2, 0.5), c(2, 3), 0.2, 1, 0.2)
 
     n.grp <- length(M)
@@ -82,6 +81,20 @@ clus.simu.2 <- function(M, rho, n, delta, pc) {
     X <- unlist(x.list)
     data.frame(X, c.id, group)
 }
+
+
+do1.clus.simu.rks.2 <- function(M, rho, n, delta, pc) {
+    dat <- clus.simu.rks.2(M, rho, n, delta, pc)
+    dat$group <- dat$group + 1
+    p1 <- cluswilcox.test(X, cluster = c.id, group = group, data = dat)$p.value
+    p2 <- cluswilcox.test(X, cluster = c.id, group = group, data = dat, method = "ds")$p.value
+    c(p1, p2)
+}
+
+power.clus.simu.rks.2 <- function(B, slevel, M, rho, n, delta, pc) {
+    dat <- t(replicate(B, do1.clus.simu.rks.2(M, rho, n, delta, pc)))
+    apply(dat, 2, function(x) mean(x < slevel))
+    }
 
 
 
@@ -181,7 +194,7 @@ clus.simu.sr1 <- function(M, n, delta, rho) {
     ## Arguments:
     ##  M: a vector or a numeric, contains the number of clusters for each size
     ##  n: a vector or a numeric, possible cluster size
-    ## rho: rho is suppposed to have the length of hte number of clusters
+    ## rho: rho is suppposed to have the length of the number of clusters
     if(length(rho) == 1) {
         rho <- rep(rho, sum(M))
     }
@@ -201,7 +214,7 @@ clus.simu.sr1 <- function(M, n, delta, rho) {
     e <- e * sqrt(1 - rho)[rep.int( 1 : sum(M), times = clus.size)]
     H <- W + e
     X <- sign(H) * exp(abs(H))
-    data.frame(X, cluster)
+    data.frame(X, c.id = cluster)
 }
 
 
@@ -213,58 +226,50 @@ clus.simu.sr2 <- function(M, n, delta, rho, phi, gamma) {
     ## Bj ~ N(0, gamma j)
     ## A single cluster size.
     ## Arguments:
-    ##  M: a vector or a numeric, contains the number of clusters for each size
-    ##  n: a vector or a numeric, possible cluster size
-    ## rho: rho is suppposed to have the length of hte number of clusters
+    ##  M:  a numeric, contains the number of clusters for each size
+    ##  n:  a numeric, possible cluster size
+    ## rho: a numeric
     ## phi: code for membrs in ea h cluster, to create non0interchangeable correlation structure
     ## gamma: variance of Bj coressponding to each code
 
-    if(length(rho) == 1) {
-        rho <- rep(rho, sum(M))
-    }
-    if(length(rho) == length(M)) {
-        rho <- rho[rep.int(1 : length(M), times = M)]
-    }
+  
     clus.size <- rep.int(n, times = M)
     cluster <- rep.int(1 : sum(M), times = clus.size)
-    
     if(length(phi) != n) {
-        stop("phi should have the length as the cluster size")
+        stop("phi should have the size of cluster size")
     }
-    if(length(gamma) != length(unique(gamma))) {
+    if(length(gamma) != length(unique(phi))) {
         stop("gamma should have the same length as the unique code in phi")
     }
     W <- rnorm(sum(M))
     W <- W[rep.int(1 : length(W), times = clus.size)]
 
-    W <- W * sqrt(rho)[rep.int(1 : sum(M), times = clus.size)]
+    W <- W * sqrt(rho)
     W <- delta + W
 
     phi.tab <- table(phi)
     phi.temp <- rep.int(1 : length(unique(phi)), times = phi.tab)
     
-    B<- rnorm( M * length(gamma)) * sqrt(gamma)
-    B.l <- length(B)
-    phi.temp <- (rep( (1 : (M)), each = n) - 1) * length(unique(phi)) + phi.temp
-    B <- B[phi.temp]
-
+    B<- rnorm(length(gamma) * M) * sqrt(gamma)
+    B <- rep.int(B, times = rep(phi.tab, M))
 
     gamma <- gamma[rep.int(1 : length(unique(phi)), times = table(phi))]
 
 
     n.obs <- sum(M * n)
     e <- rnorm(n.obs)
-    e <- e * sqrt(1 - rho - gamma)[rep.int( 1 : sum(M), times = clus.size)]
+    e <- e * sqrt(1 - rho - gamma)
     H <- W + e + B
     X <- sign(H) * exp(abs(H))
-    data.frame(X, cluster)
+    data.frame(X, c.id = cluster)
 }
 
 
 
 clus.simu.sr.info <- function(M, n, beta, delta, rho) {
-    ## Simplest data generating model in DS paper signed rank test
-    ## informative cluster size (for the sign of the cluster)
+    ##  data generating model in DS paper signed rank test with
+    ##  informative cluster size and sign.
+    ## informative cluster size (clusters with the same cluster size have the same sign)
     ## Xij = sign(Hij)exp(|Hij|)
     ## Hij = Wi + eij, Wi ~N(delta, rho_i), eij ~ N(0, 1 - rho_i)
     ## Arguments:
@@ -276,6 +281,7 @@ clus.simu.sr.info <- function(M, n, beta, delta, rho) {
     ran <- rbeta(M, 1, beta)
     cluster.size <- ifelse(ran < 0.5, n[1], n[2])
     sn <- ifelse(ran < 0.5, 1, -1)
+    sn <- rep.int(sn, times = cluster.size)
 
     M <- table(sort(cluster.size))
     n <- sort(n)
@@ -287,44 +293,91 @@ clus.simu.sr.info <- function(M, n, beta, delta, rho) {
         rho <- rho[rep.int(1 : length(M), times = M)]
     }
     
-    clus.size <- rep.int(n, times = M)
-    cluster <- rep.int(1 : sum(M), times = clus.size)
+    cluster <- rep.int(1 : sum(M), times = cluster.size)
 
     W <- rnorm(sum(M))
-    W <- W[rep.int(1 : length(W), times = clus.size)]
+  
 
-    W <- W * sqrt(rho)[rep.int(1 : sum(M), times = clus.size)]
+    W <- W * sqrt(rho)
+    W <- W[cluster]
     W <- delta + W
-    n.obs <- sum(M * n)
+    n.obs <- sum(cluster.size)
     e <- rnorm(n.obs)
-    e <- e * sqrt(1 - rho)[rep.int( 1 : sum(M), times = clus.size)]
+    e <- e * sqrt(1 - rho[cluster])
     H <- W + e
-    H <- H + e
     X <- sn * exp(abs(H))
-    data.frame(X, cluster)
+    data.frame(X, c.id = cluster)
 }
 
 
-do1.sr1 <- function(M, n, delta, rho, method = c("rgl", "ds"), paired = FALSE) {
+
+
+do1.sr1 <- function(M, n, delta, rho) {
+    ## Simplest data generating model in DS paper signed rank test
+    ## Xij = sign(Hij)exp(|Hij|)
+    ## Hij = Wi + eij, Wi ~N(delta, rho_i), eij ~ N(0, 1 - rho_i)
+    ## Arguments:
+    ##  M: a vector or a numeric, contains the number of clusters for each size
+    ##  n: a vector or a numeric, possible cluster size
+    ## rho: rho is suppposed to have the length of the number of clusters
     dat <- clus.simu.sr1(M, n, delta, rho)
-    method <- match.arg(method)
-    if(method == "rgl") {
-        pval <- cluswilcox.test(X, cluster = cluster, data = dat, paired) $ p.value
-    }
-    if(method == "ds") {
-        pval <- cluswilcox.test(X, cluster = cluster, data = dat, paired, method = "ds") $ p.value
-    }
-    return(pval)
+    p1 <- cluswilcox.test(X, cluster = c.id, data = dat, paired = TRUE) $ p.value
+    p2 <- cluswilcox.test(X, cluster = c.id, data = dat, paired = TRUE, method = "ds") $ p.value
+    c(p1, p2)
 }
 
-do.sr1 <- function(simn, M, n, delta, rho, method = c("rgl", "ds"), paired = FALSE) {
-    res <- replicate(simn, do1.sr1(M, n, delta, rho, method, paired))
-    return(res)
+power.sr1 <- function(B, slevel, M, n, delta, rho) {
+    dat <- replicate(B, do1.sr1(M, n, delta, rho))
+    return(apply(dat, 1, function(x) mean(x < slevel)))
 }
 
-power.sr1 <- function(th, simn, M, n, delta, rho, method = c("rgl", "ds"), paired = FALSE) {
-    res <- do.sr1(simn, M, n, delta, rho, method, paired)
-    power <- mean(res < th)
-    return(power)
+do1.sr2 <- function(M, n, delta, rho, phi, gamma) {
+    ## Simplest data generating model in DS paper signed rank test
+    ## intracluster ecorrelation is not exchangable
+    ## Xij = sign(Hij)exp(|Hij|)
+    ## Hij = Wi + B_phij + eij, Wi ~N(delta, rho_i), eij ~ N(0, 1 - rho_i, gamma j)
+    ## Bj ~ N(0, gamma j)
+    ## A single cluster size.
+    ## Arguments:
+    ##  M:  a numeric, contains the number of clusters for each size
+    ##  n:  a numeric, possible cluster size
+    ## rho: a numeric
+    ## phi: code for membrs in ea h cluster, to create non0interchangeable correlation structure
+    ## gamma: variance of Bj coressponding to each code
+
+    dat <- clus.simu.sr2(M, n, delta, rho, phi, gamma)
+    p1 <- cluswilcox.test(X, cluster = c.id, data = dat, paired = TRUE) $ p.value
+    p2 <- cluswilcox.test(X, cluster = c.id, data = dat, paired = TRUE, method = "ds") $ p.value
+    c(p1, p2)
+}
+
+
+power.sr2 <- function(B, slevel, M, n, delta, rho, phi, gamma) {
+    dat <- replicate(B, do1.sr2(M, n, delta, rho, phi, gamma))
+    return(apply(dat, 1, function(x) mean(x < slevel)))
+}
+
+
+do1.sr.info <- function(M, n, beta, delta, rho) {
+    ##  data generating model in DS paper signed rank test with
+    ##  informative cluster size and sign.
+    ## informative cluster size (clusters with the same cluster size have the same sign)
+    ## Xij = sign(Hij)exp(|Hij|)
+    ## Hij = Wi + eij, Wi ~N(delta, rho_i), eij ~ N(0, 1 - rho_i)
+    ## Arguments:
+    ##  M:  a numeric,  total number of clusters 
+    ##  n: a vector or a numeric, possible cluster size
+    ##  beta: a numeric, the parameter of the beta distribution for
+    ##      determine the sample size and sign of the observation
+    ##  rho: rho is suppposed to have the length of hte number of clusters
+    dat <- clus.simu.sr.info(M, n, beta, delta, rho)
+    p1 <- cluswilcox.test(X, cluster = c.id, data = dat, paired = TRUE) $ p.value
+    p2 <- cluswilcox.test(X, cluster = c.id, data = dat, paired = TRUE, method = "ds") $ p.value
+    c(p1, p2)
+}
+
+power.sr.info <- function(B, slevel, M, n, beta, delta, rho) {
+    dat <- replicate(B, do1.sr.info(M, n, beta, delta, rho))
+    return(apply(dat, 1, function(x) mean(x < slevel)))
 }
     
