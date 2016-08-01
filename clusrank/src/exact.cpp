d1e5354a968b146811f3a0e29729ba96cda9204e
Yujing Jiang <jyj.123@live.cn>
2016-07-31 21:43:40 -0400

    Added to the rank sum exact test to speed up, still need to fix the result.

--- clusrank/src/exact.cpp
+++ clusrank/src/exact.cpp
@@ -1,11 +1,13 @@
 #include <Rcpp.h>
 using namespace Rcpp;
-Rcpp::IntegerVector score_, w;
+Rcpp::IntegerVector score_, w, csize;
+int meanrks;
 //[[Rcpp::export]]
 
-int crksum(int rks, int I, int J, int sumrks, int minrks) {
-  int i, j;
-  Rcpp::IntegerVector score_sub, idx, idx_sum, score_sum;
+int crksum(int rks, int I, int J, int sumrks, int minrks, int maxrks) {
+  int i, j, csize_cum, res;
+  Rcpp::IntegerVector score_sub, idx,  score_sum, csize_ord, csize_sub;
+  /* Rcpp::Rcout <<rks <<" " <<  I << " " << J  << " " << sumrks << " " << minrks<< std::endl; */
   if (I < 0 | J < 0) return(0);    
   if (I > J) {
     rks = sumrks - rks;
@@ -22,54 +24,102 @@ int crksum(int rks, int I, int J, int sumrks, int minrks) {
     i = I;
     j = J;
   }
-  
-  if ((rks < minrks) |  (rks > (sumrks))) return(0);
+  Rcpp::Rcout << rks << " " << i << " " << j << " " << sumrks << " " << minrks << " " << maxrks << " " << std::endl;
+  if ((rks < minrks) |  (rks > (maxrks))) return(0);
+  if (rks == minrks) return(1);
   if (i == 0) {
     return( rks == 0);
   }
-  
-  if (rks < j) {
-    idx_sum = seq_len(i + rks) - 1;
-    score_sum = score_[idx_sum];
+
+  if (j > 0 ) {
+    csize_ord = Rcpp::clone(csize);
+    std::nth_element(csize_ord.begin(), csize_ord.begin() + i + j, csize_ord.end());
+    std::sort(csize_ord.begin(), csize_ord.begin() + i + j);
+    csize_sub = csize_ord[seq_len(j) - 1];
+    /*   Rcpp::Rcout << "minrks = " << minrks << " rks = " << rks << " i = " << i << " j = " << j <<  std::endl;
+        Rcpp::Rcout << "rks -mrks = " << rks - meanrks << " sum(csize_sub) = " << sum(csize_sub) << std::endl; */
+    if(((rks - meanrks) < sum(csize_sub))) {
+    csize_cum = 0;
+    int count = 0;
+    for( int ct = 0; csize_cum  < (rks - meanrks); ct = ct + 1) {
+      csize_cum += csize_ord[ct];
+      count++;
+    }
+    j = count - 1;
+    Rcpp::Rcout << " j = ? " << j << std::endl;
+    if ( j < 0 ) return(0);
+    if ( j == 0 ) {
+      idx = seq_len(i) - 1;
+      score_sub = score_[idx];
+      int temp;
+      temp = sum(score_sub);
+      Rcpp::Rcout << "j == 0" << std::endl;
+      return(rks == temp);
+    }
+    idx = seq_len(i + j) - 1;
+    score_sum = score_[idx];
     sumrks = sum(score_sum);
-    return(crksum(rks, i, rks, sumrks, minrks));
+    idx = seq_len(i) - 1 + j;
+    Rcpp::Rcout <<" count = "<< count <<" idx = " << idx <<" i= " << i << " j = " << j << std::endl; 
+        
+    score_sub = score_[idx];
+    maxrks = sum(score_sub);
+    /*  Rcpp::Rcout << rks << " " << i << " " << j << " " << sumrks << " " << minrks << " " << maxrks << " " << std::endl; */
+    return(crksum(rks, i, j, sumrks, minrks, maxrks));
+    }
+  
+    
   }
+  
+  /*   if (rks < j) {
+       idx_sum = seq_len(i + rks) - 1;
+       score_sum = score_[idx_sum];
+       sumrks = sum(score_sum);
+       return(crksum(rks, i, rks, sumrks, minrks));
+       } 
+  */
   if (j == 0) {
     return(rks == sumrks);
   } else {
     sumrks = sumrks - score_[i + j - 1];
-    
-    return(crksum(rks - score_[i+j-1], i-1, j, sumrks, minrks - score_[i - 1]) +
-           crksum(rks, i, j-1, sumrks, minrks));
+    res = crksum(rks - score_[i+j-1], i-1, j, sumrks, minrks - score_[i - 1], maxrks - score_[i + j - 1]) + crksum(rks, i, j-1, sumrks, minrks, maxrks - score_[i + j - 1] + score_[i - 1]);
+    return(res);
   }
 }
 
 //[[Rcpp::export]]
-double pcrksum(int rks, int I, IntegerVector Score) {
-  int sumrks, minrks, n, J;
+double pcrksum(int rks, int I, IntegerVector Score, IntegerVector CSize) {
+  int sumrks, minrks, maxrks, n, J;
   IntegerVector idx, score_sub;
   int N;
   double  nrksum = 0;
+  meanrks = I * (I + 1) / 2;
   score_ = Score;
+  csize = CSize;
   n = Score.size();
   J = n - I;
   N = Rf_choose(I + J, I);
   sumrks = sum(Score);
   idx = seq_len(I) - 1;
   score_sub = score_[idx];
-  minrks = sum(score_sub); /* Smallest possible rank sum*/
-  for (int k = 1; k <= rks; k++) {
-    nrksum += crksum(k, I, J, sumrks, minrks);
+  minrks = sum(score_sub); /* Smallest possible rank sum */
+  idx = idx + J;
+  score_sub = score_[idx];
+  maxrks = sum(score_sub); /* Largest possible rank sum */
+  for (int k = meanrks; k <= rks; k++) {
+    Rcpp::Rcout << "minrks = " << minrks << " rks = " << rks << " I = " << I << " J = " << J <<" k = " << k << " sumrks = " << sumrks <<    std::endl;
+    nrksum += crksum(k, I, J, sumrks, minrks, maxrks);
   }
   return(nrksum);
 }
 
 //[[Rcpp::export]]
-IntegerMatrix cumcrksum(int rks, int I, IntegerVector Score) {
+IntegerMatrix cumcrksum(int rks, int I, IntegerVector Score, IntegerVector CSize) {
   IntegerMatrix res(rks + 1, 2);
-  int sumrks, minrks, n, J;
+  int sumrks, minrks, maxrks, n, J;
   IntegerVector idx, score_sub;
 
+  meanrks = I * (I + 1) / 2;
   score_ = Score;
   n = Score.size();
   J = n - I;
@@ -77,9 +127,13 @@ IntegerMatrix cumcrksum(int rks, int I, IntegerVector Score) {
   
   idx = seq_len(I) - 1;
   score_sub = score_[idx];
-  minrks = sum(score_sub); /* Smallest possible rank sum*/
+  minrks = sum(score_sub); /* Smallest possible rank sum */
+  idx = idx + J;
+  score_sub = score_[idx];
+  maxrks = sum(score_sub); /* Largest possible rank sum */
+  
   for (int i = 0; i <= rks; i ++) {
-    res(i, 1) = crksum(i, I, J, sumrks, minrks);
+    res(i, 1) = crksum(i, I, J, sumrks, minrks, maxrks);
     res(i, 0) = i;
   }
   return(res);
